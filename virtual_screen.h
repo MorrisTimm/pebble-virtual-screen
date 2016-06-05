81ef0be3c55a565d420df469ff11f965f096e8a1
Morris <MorrisTimm@users.noreply.github.com>
2016-06-05 23:57:10 +0200

    added mode for automatic animation

--- virtual_screen.h
+++ virtual_screen.h
@@ -3,6 +3,11 @@
 
 #define VIRTUAL_SCREEN
 
-Layer* virtual_screen_init(GRect screen_bounds, GSize virtual_size);
+typedef enum {
+  VIRTUAL_SCREEN_MODE_DIRECT,
+  VIRTUAL_SCREEN_MODE_AUTO
+} VirutalScreenMode;
+
+Layer* virtual_screen_init(GRect screen_bounds, GSize virtual_size, VirutalScreenMode virtual_screen_mode);
 void virtual_screen_deinit();
 GPoint virtual_screen_get_offset();
